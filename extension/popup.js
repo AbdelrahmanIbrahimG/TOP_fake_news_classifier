5fb5f3a6d8fe4c30845850c5c83d78ccd1a924c5
ali26m <alihisham26m@gmail.com>
2024-06-11 21:26:46 +0300

    delete result.html

--- extension/popup.js
+++ extension/popup.js
@@ -1,43 +1,39 @@
 document.getElementById('check').addEventListener('click', function() {
 
-    var textareaElement = document.getElementById("text");
+    var textareaElement = document.getElementById("text").value;
+    console.log(textareaElement)
 
-    const textContent = textareaElement?.value; // Use optional chaining
-    if (textContent) {
 
-        const url = 'https://top9.p.rapidapi.com/classify';
-        const options = {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                'X-RapidAPI-Key': 'e6332296a5msh157cb1e6e200cfdp145336jsn914f220362a4',
-                'X-RapidAPI-Host': 'top9.p.rapidapi.com'
-            },
-            body: JSON.stringify({"text": textareaElement})
-        };
+    const url = 'https://top9.p.rapidapi.com/classify';
+    const options = {
+        method: 'POST',
+        headers: {
+            'x-rapidapi-key': 'e6332296a5msh157cb1e6e200cfdp145336jsn914f220362a4',
+            'x-rapidapi-host': 'top9.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"text": textareaElement})
+    };
 
-        (async () => {  
-            try {
-                const response = await fetch(url, options);
-                const result = await response.json();
-                if(result.prediction == 0){
-                    document.getElementById('result').textContent = "FALSE";
-                    document.getElementById("result").style.background = "#2b28289e";
+    (async () => {  
+        try {
+            const response = await fetch(url, options);
+            console.log(response)
+            const result = await response.json();
+            console.log(result)
+            if(result.prediction == 0){
+                document.getElementById('result').textContent = "FALSE";
+                document.getElementById("result").style.background = "#2b28289e";
 
-                }else{
-                    document.getElementById('result').textContent = "TRUE";
-                    document.getElementById("result").style.background = "#2b28289e";
-                }
-
-            } catch (error) {
-                console.error(error);
+            }else{
+                document.getElementById('result').textContent = "TRUE";
+                document.getElementById("result").style.background = "#2b28289e";
             }
-        })();
 
-    }else
-        document.getElementById('result').textContent = "Check for it";
-
-    
+        } catch (error) {
+            console.error(error);
+        }
+    })();    
 });
 
 // document.addEventListener('mouseup', function() {
--- extension/result.js
+++
@@ -1,6 +0,0 @@
-
-    // Display the result on the page
-    const result = JSON.parse(localStorage.getItem('apiResult'));
-
-    // Display the result on the page
-    document.getElementById('result').textContent = result.prediction;
